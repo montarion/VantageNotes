716cc58323d669b7767f6a3eaa918ed02370fe79
Jamiro Grantsaan <jamirograntsaan@gmail.com>
2026-02-27 00:10:27 +0100

    update serverside sqlite db on document update

--- yjs-deno-ws/persistence.ts
+++ yjs-deno-ws/persistence.ts
@@ -1,9 +1,18 @@
-import { log } from "../log.ts";
 import type { Y } from "./deps.ts";
 import * as Yd from "npm:yjs";
-
+import { ensureFile } from "jsr:@std/fs/ensure-file";
+import { MetadataExtractor } from "../common/metadata.ts";
+import { MetadataIndexer, createMetadataIndexer } from "../common/metadataindexer.ts";
+import { createServerDB } from "../common/server-db.ts";
+import { Logger } from "../common/logger.ts";
+const log = new Logger({ namespace: "yjs.persistence" });
 
 export abstract class Persistence {
+  metadataindexer: MetadataIndexer;
+  constructor(
+  ) {
+    this.metadataindexer = createMetadataIndexer(createServerDB("vantagenotes.db"))
+  }
   async load(docName: string, doc: Y.Doc): Promise<void> {
     await this.loadImpl(docName, doc);
   }
@@ -14,9 +23,32 @@ export abstract class Persistence {
 
     await this.storeUpdateImpl(docName, update);
 
-    await this.afterStoreUpdate(docName, update);
+    await this.debounceAfterStoreUpdate(docName, update);
   }
-
+  private debounceTimers = new Map<string, number>();
+  private debounceDelay = 600; // ms
+  private debounceAfterStoreUpdate(
+      docName: string,
+      update: Uint8Array
+    ) {
+    const existing = this.debounceTimers.get(docName);
+  
+    if (existing) {
+      clearTimeout(existing);
+    }
+  
+    const timeout = setTimeout(async () => {
+      try {
+        await this.afterStoreUpdate(docName, update);
+      } catch (err) {
+        console.error("afterStoreUpdate failed:", err);
+      } finally {
+        this.debounceTimers.delete(docName);
+      }
+    }, this.debounceDelay);
+  
+    this.debounceTimers.set(docName, timeout);
+  }
   protected abstract loadImpl(
     docName: string,
     doc: Y.Doc,
@@ -36,13 +68,23 @@ export abstract class Persistence {
     docName: string,
     _update: Uint8Array,
   ) {
-
+    // get document text
     const doc = new Yd.Doc();
     await this.load(docName, doc);
     const text = doc.getText(docName).toString();
+    
+    // save file to filesystem
+    const filePath = decodeURI(`./static/notes/${docName}.md`)
+    await ensureFile(filePath);
+    await Deno.writeTextFile(filePath, text);
 
+    //// update database
+    
+    //// get metadata for text
+    let metadata = await MetadataExtractor.extractMetadata(text)
+    this.metadataindexer.indexDocument(docName, metadata)
+    log.warn("UPDATED METADATA ON SERVER")
 
-    await Deno.writeTextFile(`./static/notes/${docName}.md`, text);
     
     doc.destroy();
   }
